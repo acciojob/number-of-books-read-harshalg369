4708f52eff75bec59d50d2e8b2d27bfb59e61f34
harshalg369 <hxrsh369@gmail.com>
2025-05-14 07:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
+// Sample library object
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
+  { author: "Bill Gates", title: "The Road Ahead", readingStatus: true },
+  { author: "Steve Jobs", title: "Walter Isaacson", readingStatus: true },
+  { author: "Suzanne Collins", title: "Mockingjay", readingStatus: false }
 ];
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
+// Function to count number of books read
+function numberOfBooksRead() {
+  return library.filter(book => book.readingStatus).length;
+}
 
-// Do not change the code below
+// Display result when button is clicked
+function showNumberOfBooksRead() {
+  const resultDiv = document.getElementById("result");
+  const count = numberOfBooksRead();
+  resultDiv.textContent = `📚 You have read ${count} book(s).`;
+}
